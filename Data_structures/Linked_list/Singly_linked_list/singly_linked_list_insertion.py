1c9506bc6c40cb17c901c4eba346254809f9581b
shahriar100 <shahriar.hasan100@gmail.com>
2020-08-19 16:15:52 +0600

    Singly Linked List insertion

---
+++ Data_structures/Linked_list/Singly_linked_list/singly_linked_list_insertion.py
@@ -0,0 +1,32 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+if '__name__' == __main__:
+    llist1 = LinkedList()
+    llist1.append("A")
+    llist1.append('B')
+
+    print(llist1)
