f04f5e2db91655f12c6315e363d8282925389fbc
shahriar100 <shahriar.hasan100@gmail.com>
2020-08-20 08:46:36 +0600

    Linked list node deletion

---
+++ Data_structures/Linked_list/Singly_linked_list/singly_linked_list_deletion.py
@@ -0,0 +1,64 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def print_list(self):
+        if self.head is None:
+            print("Linked list is empty")
+            return
+        itr = self.head
+        llstr = ''
+        while itr:
+            llstr += str(itr.data) + ' --> '
+            itr = itr.next
+        print(llstr)
+
+    def append(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def delete_node(self, key):
+
+        cur_node = self.head
+
+        if cur_node and cur_node.data == key:
+            self.head = cur_node.next
+            cur_node = None
+            return
+
+        prev = None
+        while cur_node and cur_node.data != key:
+            prev = cur_node
+            cur_node = cur_node.next
+
+        if cur_node is None:
+            return
+
+        prev.next = cur_node.next
+        cur_node = None
+
+
+if __name__ == "__main__":
+    llist1 = LinkedList()
+    llist1.append("A")
+    llist1.append('B')
+    llist1.append('C')
+    llist1.append('D')
+
+    llist1.print_list()
+
+    llist1.delete_node("B")
+    llist1.print_list()
